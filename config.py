2d3d9147b1436eb2fa89bfd08b2e23d247ea3c16
ramesh <ranilvannan@gmail.com>
2021-07-24 01:03:13 +0530

    First commit

---
+++ config.py
@@ -0,0 +1,23 @@
+class Config(object):
+    DEBUG = False
+    TESTING = False
+
+
+class ProductionConfig(Config):
+    MONGO_URI = "mongodb://localhost:27017/story_book"
+    MONGO_DATABASE = "story_book"
+
+    IMPORT_PATH = "/var/story_book"
+    CUSTOM_IMAGES_PATH = "/var/story_book/images"
+
+    BLOG_CODE = "Hindi"
+    MONGO_GALLERY_TABLE = "gallery"
+    MONGO_BLOG_TABLE = "hindi_blog"
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
